7d05ce8070575bc8472809f023b1f98e2ef7d681
mudouasenha <mudouasenha@gmail.com>
2022-07-23 07:13:59 -0400

    adicao inicial ao parser

--- src/parser.py
+++ src/parser.py
@@ -5,9 +5,14 @@ class CalcParser(Parser):
     # Get the token list from the lexer (required)
     tokens = CalcLexer.tokens
 
+    precedence = (
+       ('left', PLUS, MINUS),
+       ('left', TIMES, DIVIDE),
+    )
+
     # Grammar rules and actions
-    @_('expr PLUS term')
-    def expr(self, p):
+    @_('def ident(PARAMLIST){STATELIST}')
+    def FUNCDEF(self, p):
         return p.expr + p.term
 
 if __name__ == '__main__':
